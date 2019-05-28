641b4a36e9478f1d59bab0d40de981f6e41371ca
Stan Shelton <stanshelton22@gmail.com>
2019-05-28 14:41:01 -0400

    Added more photos and made some design changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 function setCopyrightDate(){
+    // Updates the Year of the copyright in the footer
     year = new Date().getFullYear();
     document.write(year);
   }
-  
-  
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;            // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}  
